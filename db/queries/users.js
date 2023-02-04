72f9e1fb5d2bb0d7d8e8982dabe96692bc6983e7
zachmanac <zachstephenson94@gmail.com>
2023-02-04 14:14:52 -0800

    add more queries, remove db as argument in query functions, linting

--- db/queries/users.js
+++ db/queries/users.js
@@ -12,7 +12,6 @@ const getUsers = () => {
 
 const getAllItems = function(limit = 6, options = {min_price: null, max_price: null}) {
   const queryParams = [];
-
   let queryString = `
     SELECT listings.*
     FROM listings
@@ -31,7 +30,7 @@ const getAllItems = function(limit = 6, options = {min_price: null, max_price: n
 
     queryParams.push(limit);
     queryString += `ORDER BY price DESC
-    LIMIT $${queryParams.length};`;
+      LIMIT $${queryParams.length};`;
 
   return db.query(queryString, queryParams)
     .then((response) => {
@@ -42,12 +41,12 @@ const getAllItems = function(limit = 6, options = {min_price: null, max_price: n
     });
 };
 
-const getFeaturedItems = function(db) {
-  const queryString = `
+const getFeaturedItems = function() {
+  let queryString = `
     SELECT listings.*
     FROM listings
     WHERE featured_status = true;
-  `;
+    `;
 
   return db.query(queryString)
     .then((response) => {
@@ -58,14 +57,14 @@ const getFeaturedItems = function(db) {
     });
 };
 
-const getUserFavorites = function(db, userId) {
+const getUserFavorites = function(userId) {
   const queryParams = [userId];
-  const queryString = `
+  let queryString = `
     SELECT listings.*
     FROM listings
     JOIN favorites ON favorites.listing_id = listings.id
     WHERE favorites.user_id = $1;
-  `;
+    `;
 
   return db.query(queryString, queryParams)
     .then((response) => {
@@ -76,17 +75,17 @@ const getUserFavorites = function(db, userId) {
     });
 };
 
-const getUserMessages = function(db, userId) {
+const getUserMessages = function(userId) {
   const queryParams = [userId];
-  const queryString = `
+  let queryString = `
     SELECT user_messages.message, sender.name, listings.title
     FROM user_messages
     JOIN users AS sender ON user_messages.sender = sender.id
     JOIN users AS receiver ON user_messages.receiver = receiver.id
     JOIN listings ON user_messages.listing = listings.id
-    WHERE sender.name = $1
+    WHERE sender.id = $1
     GROUP BY listings.title, sender.name, user_messages.message;
-  `;
+    `;
 
   return db.query(queryString, queryParams)
     .then((response) => {
@@ -97,14 +96,14 @@ const getUserMessages = function(db, userId) {
     });
 };
 
-const getAdminListings = function(db, userId) {
+const getAdminListings = function(userId) {
   const queryParams = [userId]
-  const queryString = `
+  let queryString = `
     SELECT listings.*
     FROM listings
     JOIN users on listings.owner_id = users.id
     WHERE users.id = $1;
-  `;
+    `;
 
   return db.query(queryString, queryParams)
     .then((response) => {
@@ -115,18 +114,19 @@ const getAdminListings = function(db, userId) {
     });
 };
 
-const getConversation = function(db, senderName, receiverName, listingId) {
-  const queryParams = [senderName, receiverName, listingId];
-  const queryString = `
-  SELECT user_messages.message, sender.name, receiver.name, listings.title
-  FROM user_messages
-  JOIN users AS sender ON user_messages.sender = sender.id
-  JOIN users AS receiver ON user_messages.receiver = receiver.id
-  JOIN listings ON user_messages.listing = listings.id
-  WHERE sender.id = $1
-  AND receiver.id = $2
-  AND listings.id = $3;
-  `
+const getConversation = function(senderId, receiverId, listingId) {
+  const queryParams = [senderId, receiverId, listingId];
+  let queryString = `
+    SELECT user_messages.message, sender.name, receiver.name, listings.title
+    FROM user_messages
+    JOIN users AS sender ON user_messages.sender = sender.id
+    JOIN users AS receiver ON user_messages.receiver = receiver.id
+    JOIN listings ON user_messages.listing = listings.id
+    WHERE sender.id = $1
+    AND receiver.id = $2
+    AND listings.id = $3;
+    `;
+
   return db.query(queryString, queryParams)
     .then((response) => {
       return response.rows;
@@ -140,8 +140,8 @@ const addListing = function(listing) {
   let queryParams = [listing.owner_id, listing.title, listing.description, listing.photo_1, listing.photo_2, listing.photo_3, listing.photo_4, listing.price];
 
   let queryString = `
-   INSERT INTO listings (owner_id, title, description, photo_1, photo_2,  photo_3, photo_4, price) VALUES ('$1, $2, $3, $4, $5, $6, $7, $8');
-  `;
+    INSERT INTO listings (owner_id, title, description, photo_1, photo_2,  photo_3, photo_4, price) VALUES ('$1, $2, $3, $4, $5, $6, $7, $8');
+    `;
 
   return db.query(queryString, queryParams)
     .then((response) => {
@@ -156,14 +156,78 @@ const addFavourite = function(favourite) {
   let queryParams = [favourite.user_id, favourite.listing_id];
   let queryString = `
     INSERT INTO user_favourites (user_id, listing_id) VALUES ($1, $2);
-  `;
+    `;
 
   return db.query(queryString, queryParams)
     .then((response) => {
       return response.rows;
     })
     .catch((error) => {
-      console.log(error)
+      console.log(error);
+    })
+};
+
+const createMessage = function(message) {
+  let queryParams = [message.sender, message.receiver, message.listing, message.text];
+  let queryString = `
+    INSERT INTO user_messages (sender, receiver, listing, message) VALUES ('$1, $2, $3, $4');
+    `;
+
+  return db.query(queryString, queryParams)
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+};
+
+const editSoldStatus = function(listing) {
+  const queryParams = [listing.sold_status];
+  let queryString = `
+    UPDATE listings
+    SET sold_status = TRUE
+    WHERE listings.id = $1;
+    `;
+
+  return db.query(queryString, queryParams)
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+};
+
+const deleteListing = function(listing) {
+  const queryParams = [listing.id];
+  let queryString = `
+    DELETE FROM listings
+    WHERE listings.id = $1;
+    `;
+
+  return db.query(queryString, queryParams)
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+};
+
+const deleteFavourite = function(favourite) {
+  const queryParams = [favourite.id];
+  let queryString = `
+    DELETE FROM user_favourites
+    WHERE user_favourites.id = $1;
+    `;
+
+  return db.query(queryString, queryParams)
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((error) => {
+      console.log(error);
     })
 };
 
@@ -177,25 +241,11 @@ module.exports = {
   getAdminListings,
   getConversation,
   addListing,
-  addFavourite
+  addFavourite,
+  createMessage,
+  editSoldStatus,
+  deleteListing,
+  deleteFavourite
  };
-
-
-// SQL QUERIES NON JAVASCRIPT
-
-// ADD/CREATE new message
-// INSERT INTO user_messages (sender, receiver, listing, message) VALUES ('$1, $2, $3, $4');
-
-// EDIT sold_status
-// UPDATE listings
-// SET sold_status = TRUE
-// WHERE listings.id = 1PLACEHOLDER;
-
-// DELETE listing
-// DELETE FROM listings
-// WHERE listings.id = 1PLACEHOLDER;
-
-
-
 
 
