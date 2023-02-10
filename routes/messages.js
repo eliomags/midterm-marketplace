d8511e2f75998da6c376cb659323c7c8459ca067
Elshan Magsudov <elio@jyanik.com>
2023-02-10 06:32:57 -0700

    styling, connected messages button, fixed query

--- routes/messages.js
+++ routes/messages.js
@@ -25,25 +25,25 @@ router.get("/", (req, res) => {
     });
 });
 
+/*
 // request for a specific message by product_id and user_i (buyer)
 router.get("/:listing_id/:senderId/:receiverId", (req, res) => {
   const senderId = req.params.senderId;
   const receiverId = req.params.receiverId;
   const listingId = req.params.listing_id;
 
-  console.log(senderId, receiverId, listingId, "newconsolelog", req.query)
+  console.log(senderId, receiverId, listingId, "newconsolelog", req.query);
   const userId = req.cookies.user_id;
   let priceRange = {};
   priceRange.min_price = parseInt(req.query.min_price);
   priceRange.max_price = parseInt(req.query.min_price);
 
-
   getConversation(senderId, receiverId, listingId)
     .then((conversation) => {
-      console.log(conversation, "conversation")
+      console.log(conversation, "conversation");
       res.render("conversation", {
         conversation,
-        userId,
+        userId: userId,
         senderId,
         listingId,
         receiverId,
@@ -66,6 +66,7 @@ router.get("/messages/:listing_id/new", (req, res) => {
   res.render("newmessage", { item_id, userId, priceRange });
 });
 
+/*
 // request for adding a new message
 router.post("/", (req, res) => {
   let message = {
@@ -82,11 +83,66 @@ router.post("/", (req, res) => {
     .then((result) => {
       // res.status(200).json({ result });
       // res.render("messages", { message, userId, priceRange });
-      res.redirect(`/messages/${message.listing}/${message.sender}/${message.receiver}`)
+      res.redirect(
+        `/messages/${message.listing}/${message.sender}/${message.receiver}`
+      );
     })
     .catch((error) => {
       console.log(error);
       res.status(500).json({ error: error.message });
+    });
+});
+*/
+//to check if there is an existing conversation or create a new one in the router.get for "/:listing_id/:senderId/:receiverId"
+router.get("/:listing_id/:senderId/:receiverId", (req, res) => {
+  console.log("test1");
+  const senderId = req.params.senderId;
+  const receiverId = req.params.receiverId;
+  const listingId = req.params.listing_id;
+
+  if (senderId === receiverId) {
+    return res.send("Error: Cannot send messages to yourself");
+  }
+
+  console.log(senderId, receiverId, listingId, "newconsolelog", req.query);
+  const userId = req.cookies.user_id;
+  let priceRange = {};
+  priceRange.min_price = parseInt(req.query.min_price);
+  priceRange.max_price = parseInt(req.query.min_price);
+
+  getConversation(senderId, receiverId, listingId)
+    .then((conversation) => {
+      console.log(conversation, "conversation");
+      if (conversation) {
+        res.render("conversation", {
+          conversation,
+          userId: userId,
+          senderId,
+          listingId,
+          receiverId,
+          priceRange,
+        });
+      } else {
+        createConversation(senderId, receiverId, listingId)
+          .then((newConversation) => {
+            res.render("conversation", {
+              conversation: newConversation,
+              userId: userId,
+              senderId,
+              listingId,
+              receiverId,
+              priceRange,
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+            res.send("Error");
+          });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send("Error");
     });
 });
 
